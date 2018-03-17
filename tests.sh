614637ab744615f9f57fac7f48ffe5bb0febc551
Oleg Repin <oleg.repin@iqoption.com>
2018-03-17 13:37:26 +0300

    better test logs

--- tests.sh
+++ tests.sh
@@ -11,12 +11,9 @@ docker-compose \
 echo "Waiting for tests end..."
 result=$(docker wait ci_test_internal_1)
 
+echo $(docker logs ci_test_internal_1)
 if [ $result != "0" ]
 then
-  echo "Tests exited with code 1. See:"
-  echo ""
-  echo "    docker logs ci_test_internal_1"
-  echo ""
   exit 1
 fi
 
