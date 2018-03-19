be612ed52a4b8d082e5582891dfe15e2256d4307
Oleg Repin <oleg.repin@iqoption.com>
2018-03-19 16:54:14 +0300

    one more test

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,11 +1,26 @@
 const request = require('request-promise');
+const Redis = require('ioredis');
 
 const internal = `http://internal:8888`;
+const redis = new Redis('6379', 'redis');
+
+beforeAll(() => new Promise(res => redis.on('ready', res)));
+afterAll(() => redis.quit());
 
 describe('should work', () => {
   test('should response with valid json', () =>
-    request({ uri: internal, json: true }).then(json => {
-      expect(json).toHaveProperty('visits');
-    })
+    request({ uri: internal, json: true }).then(json =>
+      expect(json).toHaveProperty('visits')
+    )
   );
+
+  test('should increment counter after request', () =>
+    redis.get('visits').then(beforeRequest =>
+      request({ uri: internal, json: true }).then(() =>
+        redis.get('visits').then(afterRequest =>
+          expect(afterRequest - beforeRequest).toBe(1)
+        )
+      )
+    )
+  )
 });
